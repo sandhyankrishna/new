74ba6816c1a0a3b9d48c09bb6fdfbe2d89170a07
sandhyankrishna <sandhyankrishna@gmail.com>
2018-09-21 11:35:44 +0530

    alert committed

--- registration.js
+++ registration.js
@@ -1,35 +1,26 @@
 $(document).ready(function() {
-
-  var $window = $(window),
-      $topOffsetTxt = $('#topOffsetTxt'),
-      $windowWTxt = $('#windowWTxt');
-
-  // Old school debuggins using Alert
-  // annoying if you have to alert a lot of data
-  // but hey, works anyway.
-  //alert('your message');
-
-  console.log('your message'); 
-  
-  // debugging code
-  $window.on('scroll', function() {
-    
-    $topOffset = $(this).scrollTop();
-	    
-    console.log($topOffset);
-    
-    $topOffsetTxt.text($topOffset+'px');
-    
-	});
-
-  $window.on('resize', function() {
-    
-    $windowWidth = $(this).width();
-    
-    console.log($windowWidth);
-    
-    $windowWTxt.text($windowWidth+'px');
-    
-  });
-  
+$("#register").click(function() {
+var name = $("#name").val();
+var email = $("#email").val();
+var password = $("#password").val();
+var cpassword = $("#cpassword").val();
+if (name == '' || email == '' || password == '' || cpassword == '') {
+alert("Please fill all fields...!!!!!!");
+} else if ((password.length) < 8) {
+alert("Password should atle 8 character in length...!!!!!!");
+} else if (!(password).match(cpassword)) {
+alert("Your passwords don't match. Try again?");
+} else {
+$.post("register.php", {
+name1: name,
+email1: email,
+password1: password
+}, function(data) {
+if (data == 'You have Successfully Registered.....') {
+$("form")[0].reset();
+}
+alert(data);
+});
+}
+});
 });
